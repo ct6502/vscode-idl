b5ba00d178a1dd7932d5fb34c38a32f368028676
Zachary Norman <zachary.norman@harris.com>
2019-08-31 19:53:13 -0600

    Client and server test framework is in place

---
+++ client/src/test/run-tests.ts
@@ -0,0 +1,25 @@
+import * as path from "path";
+import { runTests } from "vscode-test";
+
+// this routine is called when we execute "npm run test" from a command prompt
+// it starts a fresh session of vscode and runs our tests
+// another session of vscode cannot be up and running
+async function main() {
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+
+    // The path to the extension test script
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, "./index");
+
+    // Download VS Code, unzip it and run the integration test
+    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+  } catch (err) {
+    console.error("Failed to run tests");
+    process.exit(1);
+  }
+}
+
+main();
