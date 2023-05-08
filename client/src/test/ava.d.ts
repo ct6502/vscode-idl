b82988dfb204cc4a1d68a5350cd01605cf121e85
Zachary Norman <zachary.norman@harris.com>
2023-05-08 17:30:51 -0600

    WIP: Clean up unnecessary files

--- client/src/test/ava.d.ts
+++
@@ -1,9 +0,0 @@
-// workaround for https://github.com/avajs/ava/issues/2151
-// expected to be fixed in the next release
-export {};
-
-declare global {
-  interface SymbolConstructor {
-    readonly observable: symbol;
-  }
-}
--- server/src/server.interface.ts
+++
@@ -1,6 +0,0 @@
-// The server settings and defaults
-export interface IServerSettings {
-  maxNumberOfProblems: number;
-}
-
-export const DEFAULT_SERVER_SETTINGS: IServerSettings = { maxNumberOfProblems: 1000 };
