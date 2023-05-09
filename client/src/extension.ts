776f6809f63675bd0579ce053e30dc2939dc833d
Zachary Norman <zachary.norman@harris.com>
2023-05-09 09:28:18 -0600

    Tweak message

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -8,7 +8,7 @@ export async function activate(ctx: ExtensionContext) {
   // guide the users to install Microsoft Python extension.
   const newIdl = vscode.extensions.getExtension(idlName);
   if (!newIdl) {
-    vscode.window.showInformationMessage('This extension has migrated to a new place! Automatically installing the latest version, with many features and updates')
+    vscode.window.showInformationMessage('This extension has migrated to a new place! Automatically installing the latest version, with many features and updates. You should uninstall this extension.')
     await vscode.commands.executeCommand('workbench.extensions.installExtension', 'idl.idl'); // install the extension.
   }
 
