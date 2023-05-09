883743aa5dd83a1149c2745d0898412f24d2e150
Zachary Norman <zachary.norman@harris.com>
2023-05-09 09:28:01 -0600

    Update logic to check if the new IDL extension is installed

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,8 +3,14 @@ import { ExtensionContext } from 'vscode';
 
 // function to activate our extension
 export async function activate(ctx: ExtensionContext) {
+  const idlName = 'idl.idl';
 
-  await vscode.commands.executeCommand('workbench.extensions.installExtension', 'idl.idl'); // install the extension.
+  // guide the users to install Microsoft Python extension.
+  const newIdl = vscode.extensions.getExtension(idlName);
+  if (!newIdl) {
+    vscode.window.showInformationMessage('This extension has migrated to a new place! Automatically installing the latest version, with many features and updates')
+    await vscode.commands.executeCommand('workbench.extensions.installExtension', 'idl.idl'); // install the extension.
+  }
 
 }
 
